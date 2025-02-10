5e231e3f1e3c8ed8f391fd4b5fcbe51f6727cb8f
David Estes <davydotcom@gmail.com>
2025-02-10 11:42:42 -0500

    updating documentation for 8.0 and datastore type providers

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/providers/DatastoreTypeProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/providers/DatastoreTypeProvider.java
@@ -17,6 +17,7 @@
 package com.morpheusdata.core.providers;
 
 import com.bertramlabs.plugins.karman.CloudFileInterface;
+import com.morpheusdata.core.MorpheusContext;
 import com.morpheusdata.model.*;
 import com.morpheusdata.model.event.DatastoreEvent;
 import com.morpheusdata.model.event.EventType;
@@ -32,6 +33,14 @@ import java.util.List;
  * The {@link MvmProvisionFacet} allows for MVM specific provisioning tasks for MVM/HPE Hypervisor Clusters
  * In order to use this with an external integration, it is best to first register a {@link StorageProvider} plugin, then
  * ensure the code used for that provider is set on the {@link DatastoreTypeProvider#getStorageProviderCode()} method.
+ * Often times, it will be necessary to orchestrate behaviors on the host/hypervisor itself when provisioning a volume. This can be done
+ * with the context {@link MorpheusContext#executeCommandOnServer} method.
+ * <p>
+ *     <code>
+ *         ComputeServer host = server.parentServer //this is the host
+ *         ServiceResponse response = morpheusContext.executeCommandOnServer(host, "virsh pool-refresh mypool")
+ *     </code>
+ * </p>
  * @since 1.1.18
  * @author David Estes, Dan Devilbiss
  */
@@ -110,7 +119,36 @@ public interface DatastoreTypeProvider extends PluginProvider {
 	 */
 	ServiceResponse<StorageVolume> createVolume(StorageVolume volume, ComputeServer server);
 
+	/**
+	 * Clones a volume based on a source volume object. This is one of the most important methods for provisioning as most
+	 * {@link VirtualImage} provisioning objects are cloned from a local image cache of source volumes. This is where the QCOW2 may reside
+	 * Often times you can infer this from the combination of the sourceVolume object as well as its datastore
+	 * <p>
+	 *     <code>
+	 *         String sourceVolumePath = sourceVolume.datastore.externalPath + '/' + sourceVolume.externalId
+	 *         //this is the QCOW2 path typically used for clone operations
+	 *         String command = "sudo mkdir -p \"${volume.datastore.externalPath}/${server.externalId ?: server.name}\" ; sudo ionice -c 3 cp -f \"${sourceVolume.datastore.externalPath}/${sourceVolume.externalId}\" \"${volume.datastore.externalPath}/${server.externalId ?: server.name}/${volume.volumeName}\""
+	 *		   morpheusContext.executeCommandOnServer(server, command)
+	 *     </code>
+	 * </p>
+	 * @param volume the volume we are creating and cloning into
+	 * @param server the server the volume is associated with (typically the workload/vm)
+	 * @param sourceVolume the source volume we are cloning from
+	 * @return the success state and a copy of the volume
+	 */
 	ServiceResponse<StorageVolume> cloneVolume(StorageVolume volume, ComputeServer server, StorageVolume sourceVolume);
+
+	/**
+	 * Clones a volume based on a source being the reference to the actual File in the Virtual Image. This can be called in the event there is no image cache or we need to directly stream to an image target.
+	 * Remember, this code runs in the manager or morpheus appliance and not on the host itself. In order to stream contents directly to the target , we need to create a link we can fetch using the {@link com.morpheusdata.core.MorpheusFileCopyService}
+	 * @see com.morpheusdata.core.MorpheusFileCopyService
+	 * @see com.morpheusdata.core.synchronous.MorpheusSynchronousFileCopyService
+	 * @param volume the volume we are creating and cloning into
+	 * @param server the server the volume is associated with (typically the workload/vm)
+	 * @param virtualImage the virtual image this volume is being cloned out of
+	 * @param cloudFile the specific disk file (Karman abstraction) that is being cloned
+	 * @return the success state and a copy of the volume
+	 */
 	ServiceResponse<StorageVolume> cloneVolume(StorageVolume volume, ComputeServer server, VirtualImage virtualImage, CloudFileInterface cloudFile);
 
 	/**
@@ -152,36 +190,92 @@ public interface DatastoreTypeProvider extends PluginProvider {
 
 	/**
 	 * Perform any operations necessary on the target to create a snapshot of a volume. In order to use this simply
-	 * implement this interface along with the {@link DatastoreTypeProvider}
-	 *
+	 * implement this interface along with the {@link DatastoreTypeProvider}. If using along with VME/MVM, it is best to
+	 * implement the {@link SnapshotFacet.SnapshotServerFacet} instead.
+	 * @see Snapshot
+	 * @see SnapshotFile
 	 * @since 1.1.18
 	 * @author David Estes, Dan Devilbiss
 	 */
 	public interface SnapshotFacet {
 
+		/**
+		 * Creates a snapshot of a volume. This is used to create a snapshot of a volume. A lot of backup providers may use this to create snapshots for exportability or backup purposes.
+		 * NOTE: Currently this isn't really used by anything, as the server is normally associated with most snapshot tracking
+		 * @param volume the volume to create a snapshot for
+		 * @return the success state and a copy of the snapshot
+		 */
 		ServiceResponse<Snapshot> createSnapshot(StorageVolume volume);
+
+		/**
+		 * Reverts a volume to a snapshot. This is used to revert a volume to a previous state.
+		 * @param volume
+		 * @return
+		 */
 		ServiceResponse removeSnapshot(StorageVolume volume);
 
+		/**
+		 * Called to list all snapshots on an associated storage server (NOT USED YET)
+		 * @param storageServer the storage server to list snapshots off of
+		 * @return the success state of the listing
+		 */
 		ServiceResponse<Snapshot> listSnapshots(StorageServer storageServer);
 		ServiceResponse<StorageVolume> cloneVolume(StorageVolume volume, Snapshot sourceSnapshot);
 
 		/**
 		 * This is a more refined Snapshot facet that is used for Compute Servers. This is used to create and revert snapshots.
-		 * Sometimes snapshots have to be part of a server as a whole and cannot be per volume. This allows for that implementation style
+		 * Sometimes snapshots have to be part of a server as a whole and cannot be per volume. This allows for that implementation style.
+		 * NOTE: For VME/MVM Cluster types, this is the facet you want to implement.
 		 *
 		 * @since 1.1.18
 		 * @author David Estes, Dan Devilbiss
 		 */
 		public interface SnapshotServerFacet {
+			/**
+			 * Creates volume snapshots of all volumes associated with a server. If the server volume is of type 'cdrom',
+			 * it is best to not snapshot these as ISO formats are not typically worth snapshotting
+			 * A lot of backup providers may use this to create snapshots for exportability or backup purposes.
+			 * In those scenarios, it is important to create exportPath information on the {@link SnapshotFile} so the
+			 * morphd agent can fetch the snapshot and store it in the backup provider.
+			 * @param server the server to create snapshots for (typically a vm). Host can be acquired via {@link ComputeServer#getParentServer()}
+			 * @param forBackup whether this snapshot is for backup purposes
+			 * @param forExport whether this snapshot is for export purposes (like an image import)
+			 * @return the success state and a copy of the snapshot
+			 */
 			ServiceResponse<Snapshot> createSnapshot(ComputeServer server, Boolean forBackup, Boolean forExport);
+
+			/**
+			 * Reverts a server to a snapshot. This is used to revert a server to a previous state. MVM/VME will automatically
+			 * ensure the server is powered off during this operation and powered back on to desired user state after this
+			 * operation is complete. NOTE: These snapshots are typically volume based and not vm state based.
+			 * @param server the server to revert the snapshot on
+			 * @param snapshot the snapshot to revert to
+			 * @return the success state and a copy of the snapshot
+			 */
 			ServiceResponse<Snapshot> revertSnapshot(ComputeServer server, Snapshot snapshot);
+
+			/**
+			 * Removes a snapshot from a server. This is used to remove a snapshot from a server. Sometimes,
+			 * this means snapshots have to be consolidated or flattened depending on the implementation.
+			 * @param server the server to remove the snapshot from
+			 * @param snapshot the snapshot to remove
+			 * @return the success state of the removal
+			 */
 			ServiceResponse removeSnapshot(ComputeServer server, Snapshot snapshot);
 		}
 	}
 
 	/**
-	 * This facet is used to provide additional hooks for MVM specific provisioning tasks for MVM/HPE Hypervisor Clusters
+	 * This facet is used to provide additional hooks for MVM/VME specific provisioning tasks for VME/HPE Hypervisor Clusters.
+	 * In the future, this may change to be  more generic and allowed for other cloud provision providers to use (i.e. Baremetal)
 	 * In order to use this, add it to your implementation of your {@link DatastoreTypeProvider}
+	 * Often times, it will be necessary to orchestrate behaviors on the host itself when provisioning a volume. This can be done
+	 * with the context {@link MorpheusContext#executeCommandOnServer} method.
+	 * <p>
+	 *     <code>
+	 *         ServiceResponse response = morpheusContext.executeCommandOnServer(host, "virsh pool-refresh mypool");
+	 *     </code>
+	 * </p>
 	 * @since 1.1.18
 	 * @author David Estes , Dan Devilbiss
 	 */
@@ -210,7 +304,7 @@ public interface DatastoreTypeProvider extends PluginProvider {
 		/**
 		 * This is a hook call to allow the plugin to know if a vm is being moved off of a host or removed. It should not be used to remove volume
 		 * but rather if there is work to be done to release the volume from the host. This could be something like forcing a rescan.
-		 * @param cluster
+		 * @param cluster reference to the current cluster this is being run within
 		 * @param volume
 		 * @return
 		 */
@@ -219,6 +313,10 @@ public interface DatastoreTypeProvider extends PluginProvider {
 		/**
 		 * This represents a disk configuration for a virtual machine for libvirt/virsh XML specification as a disk device
 		 * This is used to allow the plugin to override any special configuration needed for the disk device
+		 * Please refer to the libvirt documentation on block devices:
+		 * <p>
+		 *     <a href="https://libvirt.org/formatdomain.html#devices">https://libvirt.org/formatdomain.html#devices</a>
+		 * </p>
 		 * @author David Estes, Dan Devilbiss
 		 */
 		public static class MvmDiskConfig {
@@ -253,6 +351,7 @@ public interface DatastoreTypeProvider extends PluginProvider {
 	 * it may be necessary to perform certain optimization operations on a datastore when a vm is moved between hosts, or to
 	 * notify a datastore provider when a vm is inactive (powered off) to perform certain operations.
 	 * @see DatastoreEvent
+	 * @author David Estes
 	 */
 	public interface DatastoreEventFacet extends GenericIntegrationProvider.EventSubscriberFacet<DatastoreEvent> {
 		@Override
