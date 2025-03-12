d6db312c1b10f5321704063cbe5be6c8e744819a
Alex Clement <MrClement@users.noreply.github.com>
2025-03-12 14:47:33 -0600

    ADd defaults for getCloudProviderCode and getGenericProviderCode

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/providers/NetworkProvider.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/providers/NetworkProvider.java
@@ -53,13 +53,13 @@ public interface NetworkProvider extends PluginProvider {
 	 * NetworkServer of this type attached to them. Network actions will then be handled via this provider.
 	 * @return String Code of the Cloud type
 	 */
-	String getCloudProviderCode();
+	default String getCloudProviderCode() {return null;}
 
 	/**
 	 * The GenericProvider code that this NetworkProvider should be attached to. Should be used when there is no CloudProvider.
 	 * @return String Code of the Cloud type
 	 */
-	String getGenericProviderCode();
+	default String getGenericProviderCode() {return null;}
 
 
 	/**
