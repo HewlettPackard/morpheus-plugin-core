8abb2668b6db2e7203efaf725ec21f776c9568aa
Mike Carlin <mike@mcarlin.com>
2025-05-08 09:28:30 -0700

    [PCCP-574] Rework process service APIs
    - Rework the process service APIs to support custom step types
    - Add updateStep method

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusProcessService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusProcessService.java
@@ -27,6 +27,16 @@ import io.reactivex.rxjava3.core.Single;
  * notify Morpheus that various actions are currently being performed.
  */
 public interface MorpheusProcessService extends MorpheusDataService<Process, Process> {
+	/**
+	 * When the status is set to STATUS_COMPLETE, the step will show as complete and the message
+	 * field is shown in the history detail modal.
+	 */
+	String STATUS_COMPLETE = "complete";
+	/**
+	 * When the status is set to STATUS_FAILED, the step will show as failed and the error filed
+	 * will be shown in the history detail modal.
+	 */
+	String STATUS_FAILED = "failed";
 
 	/**
 	 * Start a new Process for the Workload
@@ -35,8 +45,19 @@ public interface MorpheusProcessService extends MorpheusDataService<Process, Pro
 	 * @param user the User that starts the process (optional)
 	 * @param timerCategory a category to associate with this Process
 	 * @return Boolean indicating success
+	 * @deprecated use {@link #startProcess(Workload, ProcessStepType, User, String)} instead
 	 */
 	Single<Process> startProcess(Workload workload, ProcessEvent.ProcessType processType, User user, String timerCategory);
+
+	/**
+	 * Start a new Process for the Workload
+	 * @param workload the Workload to associate the Process to
+	 * @param stepType the ProcessStepType to start
+	 * @param user the User that starts the process (optional)
+	 * @param timerCategory a category to associate with this Process
+	 * @return Boolean indicating success
+	 */
+	Single<Process> startProcess(Workload workload, ProcessStepType stepType, User user, String timerCategory);
 
 	/**
 	 * Start a new Process for the Workload
@@ -46,8 +67,22 @@ public interface MorpheusProcessService extends MorpheusDataService<Process, Pro
 	 * @param timerCategory a category to associate with this Process
 	 * @param eventTitle an event title to associate with this Process
 	 * @return Boolean indicating success
+	 * @deprecated use {@link #startProcess(Workload, ProcessStepType, User, String, String)} instead
 	 */
+	@Deprecated(since = "1.2.8")
 	Single<Process> startProcess(Workload workload, ProcessEvent.ProcessType processType, User user, String timerCategory, String eventTitle);
+
+
+	/**
+	 * Start a new Process for the Workload
+	 * @param workload the Workload to associate the Process to
+	 * @param stepType the ProcessStepType to start
+	 * @param user the User that starts the process (optional)
+	 * @param timerCategory a category to associate with this Process
+	 * @param eventTitle an event title to associate with this Process
+	 * @return Boolean indicating success
+	 */
+	Single<Process> startProcess(Workload workload, ProcessStepType stepType, User user, String timerCategory, String eventTitle);
 
 	/**
 	 * Start a new ProcessEvent associated to the Process. This will end any currently running
@@ -67,11 +102,38 @@ public interface MorpheusProcessService extends MorpheusDataService<Process, Pro
 	 * @param nextEvent The new ProcessEvent to start
 	 * @param processStatus The status (i.e. 'complete', 'failed')
 	 * @return Boolean indicating success
+	 * @deprecated Use {@link #startProcessStepAt(Process, ProcessStepType, ProcessEvent, String)}
 	 */
+	@Deprecated(since = "1.2.8")
 	Single<Boolean> startProcessStepAt(Process process, ProcessEvent.ProcessType lastStepType, ProcessEvent nextEvent, String processStatus);
 
 	/**
+	 * Start a new ProcessEvent associated to the Process while ending a specific previous step.
+	 * This will end the ProcessEvent with the specified lastStepType and then start a new ProcessEvent.
+	 * @param process The Process on which to create a new ProcessEvent to start
+	 * @param lastStepType The type of the last step to end before starting the new event
+	 * @param nextStep The new ProcessEvent to start
+	 * @param processStatus The status (i.e. 'complete', 'failed')
+	 * @return Boolean indicating success
+	 */
+	Single<Boolean> startProcessStepAt(Process process, ProcessStepType lastStepType, ProcessEvent nextStep, String processStatus);
+
+	/**
+	 * Updates the last ProcessEvent of type `lastStepType`
+	 * @param process The process to search for an event of type `lastStepType`
+	 * @param lastStepType type of the last event to update
+	 * @param stepUpdate update parameters for the event
+	 * @param appendOutput true to append to the existing output; false to replace
+	 * @return Boolean indicating success
+	 */
+	Single<Boolean> updateProcessStep(Process process, ProcessStepType lastStepType, ProcessStepUpdate stepUpdate, Boolean appendOutput) ;
+
+	/**
 	 * Stops the last ProcessEvent associated with the Process
+	 * <p>
+	 * This will not appendOutput and will instead override it. If you want to preserve the existing output, use
+	 * {@link #endProcessStep(Process, String, String, Boolean)} which allows you to specify appendOutput = true.
+	 *
 	 * @param process The Process from which to fetch the last ProcessEvent to stop
 	 * @param processStatus The status (i.e. 'complete', 'failed')
 	 * @param output The output to associate with the ProcessEvent
@@ -80,14 +142,42 @@ public interface MorpheusProcessService extends MorpheusDataService<Process, Pro
 	Single<Boolean> endProcessStep(Process process, String processStatus, String output);
 
 	/**
+	 * Stops the last ProcessEvent associated with the Process
+	 * @param process The Process from which to fetch the last ProcessEvent to stop
+	 * @param processStatus The status (i.e. 'complete', 'failed')
+	 * @param output The output to associate with the ProcessEvent
+	 * @param appendOutput true to append to the existing output; false to replace
+	 * @return Boolean indicating success
+	 */
+	Single<Boolean> endProcessStep(Process process, String processStatus, String output, Boolean appendOutput);
+
+
+	/**
+	 * Stops a specific ProcessEvent associated with the Process.
+	 * <p>
+	 * This will not appendOutput and will instead override it. If you want to preserve the existing output, use
+	 * {@link #endProcessStep(Process, String, String, Boolean)} which allows you to specify appendOutput = true.
+	 *
+	 * @param process The Process from which to fetch the last ProcessEvent to stop
+	 * @param lastStepType The type of the last step to end
+	 * @param processStatus The status (i.e. 'complete', 'failed'). If not specified, 'complete' is used.
+	 * @param output The output to associate with the ProcessEvent
+	 * @return Boolean indicating succes
+	 * @deprecated Use {@link #endProcessStepAt(Process, ProcessStepType, String, String, Boolean)}
+	 */
+	@Deprecated(since = "1.2.8")
+	Single<Boolean> endProcessStepAt(Process process, ProcessEvent.ProcessType lastStepType, String processStatus, String output);
+
+	/**
 	 * Stops a specific ProcessEvent associated with the Process.
 	 * @param process The Process from which to fetch the last ProcessEvent to stop
 	 * @param lastStepType The type of the last step to end
-	 * @param processStatus The status (i.e. 'complete', 'failed')
+	 * @param processStatus The status (i.e. 'complete', 'failed'). If not specified, 'complete' is used.
 	 * @param output The output to associate with the ProcessEvent
+	 * @param appendOutput true to append to the existing output; false to replace
 	 * @return Boolean indicating success
 	 */
-	Single<Boolean> endProcessStepAt(Process process, ProcessEvent.ProcessType lastStepType, String processStatus, String output);
+	Single<Boolean> endProcessStepAt(Process process, ProcessStepType lastStepType, String processStatus, String output, Boolean appendOutput);
 
 	/**
 	 * End the process with the status specified
