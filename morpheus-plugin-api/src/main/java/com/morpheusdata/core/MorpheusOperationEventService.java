01c17b16be41a8213981e2033b4e6d97f0e0c2b1
Jay Guadagnoli <jay.guadagnoli@hpe.com>
2025-08-19 09:55:28 -0600

    removed the listIdentityProjections and sync.
     Users should use the dataquery style from MorpheusIdentityService.

--- morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusOperationEventService.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/MorpheusOperationEventService.java
@@ -6,19 +6,4 @@ import io.reactivex.rxjava3.core.Observable;
 
 public interface MorpheusOperationEventService extends MorpheusDataService<OperationEvent, OperationEventIdentityProjection>, MorpheusIdentityService<OperationEventIdentityProjection> {
 
-	/**
-	 * Get a list of OperationEvent projections based on a category name
-	 * @param category category string unique filter category
-	 * @return Observable stream of sync projection
-	 */
-	Observable<OperationEventIdentityProjection> listIdentityProjections(String category);
-
-	/**
-	 * Get a list of OperationEvent projections based on a category name
-	 * @param category category
-	 * @return Observable stream of sync projection
-	 * @deprecated replaced by {{@link #listIdentityProjections(String)}}
-	 */
-	@Deprecated
-	Observable<OperationEventIdentityProjection> listSyncProjections(String category);
 }
