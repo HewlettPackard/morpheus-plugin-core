226777e53b633b1e41ae28092d48bc0905643a1e
saadmansoor93 <saad-mansoor93@hotmail.com>
2025-09-30 16:17:03 -0500

    [PCCP-6125] add custom plugin info support

---
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/core/providers/ResourceViewUIFacet.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2024 Morpheus Data, LLC.
+ *
+ * Licensed under the PLUGIN CORE SOURCE LICENSE (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://raw.githubusercontent.com/gomorpheus/morpheus-plugin-core/v1.0.x/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.morpheusdata.core.providers;
+
+import com.morpheusdata.model.*;
+import java.util.List;
+
+/**
+ * Provides support for custom detail entries in resource Info sections.
+ * This interface allows plugins to inject custom information into existing
+ * detail views without requiring a full tab implementation.
+ *
+ * @param <T> The type of MorpheusModel this facet applies to
+ * @author Morpheus Development Team
+ * @since 0.16.0
+ */
+public interface ResourceViewUIFacet<T extends MorpheusModel> extends UIExtensionProvider {
+
+	/**
+	 * Get custom details from the plugin to render in the relevant info section
+	 * @param model The model instance to get details for
+	 * @return List of DetailEntry objects to display in the Info section
+	 */
+	List<DetailEntry> getDetailViewInfo(T model);
+
+	/**
+	 * Provide logic to determine when these details should be displayed.
+	 * This logic is checked after permissions are validated.
+	 *
+	 * @param model The model instance
+	 * @param user Current User details
+	 * @param account Account details
+	 * @return whether the details should be displayed
+	 */
+	Boolean show(T model, User user, Account account);
+}
---
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/DetailEntry.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2024 Morpheus Data, LLC.
+ *
+ * Licensed under the PLUGIN CORE SOURCE LICENSE (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://raw.githubusercontent.com/gomorpheus/morpheus-plugin-core/v1.0.x/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.morpheusdata.model;
+
+/**
+ * Represents a single detail entry that can be displayed in the Info section of a resource view.
+ * This allows plugins to inject custom information into existing detail views without requiring
+ * a full tab implementation.
+ *
+ * @author Morpheus Development Team
+ * @since 0.16.0
+ */
+public class DetailEntry {
+
+	/**
+	 * The internationalized label key for this detail entry.
+	 * This should be used first if i18n translation is available.
+	 */
+	public String i18nLabel;
+
+	/**
+	 * The fallback label to use if i18n translation is not available.
+	 * This will be displayed as-is if i18nLabel cannot be resolved.
+	 */
+	public String fallbackLabel;
+
+	/**
+	 * The value to display for this detail entry.
+	 * This will be rendered as a string in the UI.
+	 */
+	public String value;
+
+	/**
+	 * Default constructor
+	 */
+	public DetailEntry() {
+	}
+
+	/**
+	 * Constructor with all fields
+	 * @param i18nLabel The internationalized label key
+	 * @param fallbackLabel The fallback label if i18n is unavailable
+	 * @param value The value to display
+	 */
+	public DetailEntry(String i18nLabel, String fallbackLabel, String value) {
+		this.i18nLabel = i18nLabel;
+		this.fallbackLabel = fallbackLabel;
+		this.value = value;
+	}
+
+	/**
+	 * Constructor with fallback label and value only
+	 * @param fallbackLabel The label to display
+	 * @param value The value to display
+	 */
+	public DetailEntry(String fallbackLabel, String value) {
+		this.fallbackLabel = fallbackLabel;
+		this.value = value;
+	}
+}
