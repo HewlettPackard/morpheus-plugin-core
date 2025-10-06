2610a521f5c98d829d66c0e29fd061946a2cd068
Jordon Saardchit <jordon.saardchit@hpe.com>
2025-10-06 15:12:38 -0700

    Adding a buncha crap for bgp plugin support

---
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/projection/NetworkRouterBgpNeighborIdentityProjection.java
@@ -0,0 +1,29 @@
+package com.morpheusdata.model.projection;
+
+import com.morpheusdata.model.MorpheusModel;
+
+public class NetworkRouterBgpNeighborIdentityProjection extends MorpheusModel {
+	protected String externalId;
+	protected String refType;
+	protected Long refId;
+
+	public NetworkRouterBgpNeighborIdentityProjection() {}
+
+	public NetworkRouterBgpNeighborIdentityProjection(String externalId, String refType, Long refId) {
+		this.externalId = externalId;
+		this.refType = refType;
+		this.refId = refId;
+	}
+
+	public String getExternalId() { return externalId; }
+
+	public void setExternalId(String externalId) { this.externalId = externalId; }
+
+	public String getRefType() { return refType; }
+
+	public void setRefType(String refType) { this.refType = refType; }
+
+	public Long getRefId() { return refId; }
+
+	public void setRefId(Long refId) { this.refId = refId; }
+}
