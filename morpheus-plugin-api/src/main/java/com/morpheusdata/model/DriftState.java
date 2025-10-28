a34aa7b5f5c16b8c5ad1be9f34bb54457e3a83f7
Karanth <gowtam.chandra.karanth@hpe.com>
2025-10-28 21:04:35 +0530

    additional fields

--- morpheus-plugin-api/src/main/java/com/morpheusdata/model/DriftState.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/model/DriftState.java
@@ -18,8 +18,8 @@ public class DriftState {
     protected DriftCheckState state;     // status of the drift check (e.g., pending, in-progress, failed, completed)
     protected Integer percentComplete = 0; // completion percentage
 
-    private Instant startedOn;            // start timestamp
-    private Instant completedOn;          // completion timestamp
+    private Instant startedAt;            // start timestamp
+    private Instant completedAt;          // completion timestamp
 
     protected DriftStatus driftStatus;   // status of the drift check (e.g., no-drift, drift-detected, error)
     protected List<DriftRuleResult> ruleResults = new ArrayList<>(); // list of rules executed and their status
@@ -95,14 +95,11 @@ public class DriftState {
     public Integer getPercentComplete() { return percentComplete; }
     public void setPercentComplete(Integer percentComplete) { this.percentComplete = percentComplete; }
 
-    public String getLastUpdatedOn() { return lastUpdatedOn; }
-    public void setLastUpdatedOn(String lastUpdatedOn) { this.lastUpdatedOn = lastUpdatedOn; }
+    public Instant getStartedAt() { return startedAt; }
+    public void setStartedOn(Instant startedAt) { this.startedAt = startedAt; }
 
-    public Instant getStartedOn() { return startedOn; }
-    public void setStartedOn(Instant startedOn) { this.startedOn = startedOn; }
-
-    public Instant getCompletedOn() { return completedOn; }
-    public void setCompletedOn(Instant completedOn) { this.completedOn = completedOn; }
+    public Instant getCompletedAt() { return completedAt; }
+    public void setCompletedAt(Instant completedAt) { this.completedAt = completedAt; }
 
     public DriftStatus getDriftStatus() { return driftStatus; }
     public void setDriftStatus(DriftStatus driftStatus) { this.driftStatus = driftStatus; }
