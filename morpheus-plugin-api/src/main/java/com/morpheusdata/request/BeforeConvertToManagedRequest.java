9b3a4e9a089bdedd6dda9f88e16a037b6428ba9e
Patricia DiBenedetto <patricia.dibenedetto@hpe.com>
2026-06-26 17:07:05 -0500

    intial code

--- morpheus-plugin-api/src/main/java/com/morpheusdata/request/BeforeConvertToManagedRequest.java
+++ morpheus-plugin-api/src/main/java/com/morpheusdata/request/BeforeConvertToManagedRequest.java
@@ -1,9 +1,17 @@
 package com.morpheusdata.request;
 
 import com.morpheusdata.model.ComputeServer;
+import com.morpheusdata.model.Instance;
 
 public class BeforeConvertToManagedRequest {
 	public ComputeServer server;
+
+	/**
+	 * The instance this server is being added to. Set only for the <em>add-node</em> (scale-instance)
+	 * flow; {@code null} for the <em>convert-to-managed</em> flow.
+	 * Plugins can use the presence of this field to distinguish between the two operations.
+	 */
+	public Instance instance;
 
 	public ComputeServer getServer() {
 		return server;
@@ -11,5 +19,13 @@ public class BeforeConvertToManagedRequest {
 
 	public void setServer(ComputeServer server) {
 		this.server = server;
+	}
+
+	public Instance getInstance() {
+		return instance;
+	}
+
+	public void setInstance(Instance instance) {
+		this.instance = instance;
 	}
 }
